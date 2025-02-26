d856892d5eb6a2b43e39c5bbae777134ccfe7ff2
Alfarizy <rizy.izy15@gmail.com>
2025-02-26 11:09:03 +0100

    Add Dashboard component with logout functionality and new job dialog

---
+++ thesis_project/transpara/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,45 @@
+// src/components/Dashboard/Dashboard.tsx
+import React, { useState } from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { TransparaAppBar } from "../AppBar/TransparaAppBar";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
+import { NewJobDialog } from "./NewJob"; // <-- We'll create this
+
+const Dashboard: React.FC = () => {
+  const [openNewJob, setOpenNewJob] = useState(false);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <Box>
+      <TransparaAppBar onLogout={handleLogout} />
+      <Box sx={{ p: 3 }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          Dashboard Content
+        </Typography>
+
+        {/* Button to open the "New Job" dialog */}
+        <Button
+          variant="contained"
+          onClick={() => setOpenNewJob(true)}
+          sx={{
+            backgroundColor: "black",
+            "&:hover": {
+              backgroundColor: "#333",
+            },
+          }}
+        >
+          New Job
+        </Button>
+      </Box>
+
+      {/* Dialog for creating a new job */}
+      <NewJobDialog open={openNewJob} onClose={() => setOpenNewJob(false)} />
+    </Box>
+  );
+};
+
+export default Dashboard;
