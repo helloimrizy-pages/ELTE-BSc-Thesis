f04e55b819ffee17e15dfc5a27853f291e351203
Alfarizy <rizy.izy15@gmail.com>
2025-04-04 06:06:49 +0200

    Add rank_candidates and display_ranking to models module exports

--- transpara/resume_ranking/src/models/__init__.py
+++ transpara/resume_ranking/src/models/__init__.py
@@ -1,6 +1,8 @@
 from src.models.ranking_model import (
     load_ranking_model,
     predict_with_ranking_model,
+    rank_candidates,
+    display_ranking
 )
 
 from src.models.embedding_debiasing import (
@@ -19,6 +21,8 @@ from src.models.linguistic_debiasing import (
 __all__ = [
     'load_ranking_model',
     'predict_with_ranking_model',
+    'rank_candidates',
+    'display_ranking',
     'compute_gender_subspace',
     'debias_embedding',
     'cosine_similarity',
